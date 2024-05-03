968109fccffe9a23d66dbc0b1acf0eac7d45bcd2
David Natingga <info@algohertz.com>
2024-05-03 09:26:43 +0200

    Add teleport to teleportation example

--- src/bin/teleportation.rs
+++ src/bin/teleportation.rs
@@ -15,6 +15,31 @@ use quantum_simulation::simulation::QuantumSimulation;
 const QUBIT_COUNT: usize = 3;
 const RUN_COUNT: usize = 1000;
 
+// Teleport the quantum information from the source qubit into the destination qubit using an auxiliary qubit.
+fn teleport(
+    simulation: &mut QuantumSimulation,
+    src_qubit: usize,
+    aux_qubit: usize,
+    dest_qubit: usize,
+) {
+    // Entangle qubits 1 and 2.
+    simulation.hadamard(aux_qubit);
+    simulation.cnot(aux_qubit, dest_qubit);
+
+    // Teleport the quantum information from qubit 0 into qubit 2.
+    simulation.cnot(src_qubit, aux_qubit);
+    simulation.hadamard(src_qubit);
+    let measured_states = simulation.measure(vec![src_qubit, aux_qubit]);
+    let first_bit = measured_states[0];
+    let second_bit = measured_states[1];
+    if first_bit {
+        simulation.pauli_z(dest_qubit);
+    }
+    if second_bit {
+        simulation.pauli_x(dest_qubit);
+    }
+}
+
 fn main() {
     println!("Teleportation:");
     let mut simulation = QuantumSimulation::new(QUBIT_COUNT, 0u64);
@@ -25,22 +50,7 @@ fn main() {
         // Prepare qubit 0. It will be teleported.
         simulation.hadamard(0);
 
-        // Entangle qubits 1 and 2.
-        simulation.hadamard(1);
-        simulation.cnot(1, 2);
-
-        // Teleport the quantum information from qubit 0 into qubit 2.
-        simulation.cnot(0, 1);
-        simulation.hadamard(0);
-        let measured_states = simulation.measure(vec![0, 1]);
-        let first_bit = measured_states[0];
-        let second_bit = measured_states[1];
-        if first_bit {
-            simulation.pauli_z(2);
-        }
-        if second_bit {
-            simulation.pauli_x(2);
-        }
+        teleport(&mut simulation, 0, 1, 2);
 
         let measured_states = simulation.measure(vec![2]);
         measurements.push(measured_states);
