5c642389c2ae469db8e8e6a847bd6a5ef86707d7
David Natingga <info@algohertz.com>
2024-05-03 09:34:03 +0200

    Refactor bell_state

--- src/bin/bell_state.rs
+++ src/bin/bell_state.rs
@@ -16,21 +16,23 @@ use quantum_simulation::simulation::QuantumSimulation;
 const QUBIT_COUNT: usize = 2;
 const RUN_COUNT: usize = 100;
 
-fn produce_ghz_state(run_count: usize) {
-    let mut simulation = QuantumSimulation::new(QUBIT_COUNT, 0u64);
-    let mut measurements = Vec::with_capacity(RUN_COUNT);
-    for _ in 0..run_count {
-        simulation.init_ground_state();
-        simulation.hadamard(0);
-        simulation.cnot(0, 1);
-        let measured_states = simulation.measure_all();
-        measurements.push(measured_states);
-    }
-    evaluate(measurements);
+// Entangle two qubits into a Bell state (1/sqrt(2))*(|00⟩ + |11⟩).
+// Assume that the qubits are initialized to the state |00⟩.
+fn entangle_into_bell_state(simulation: &mut QuantumSimulation, qubit0: usize, qubit1: usize) {
+    simulation.hadamard(qubit0);
+    simulation.cnot(qubit0, qubit1);
 }
 
 fn main() {
     println!("Bell state:");
-    produce_ghz_state(RUN_COUNT);
+    let mut simulation = QuantumSimulation::new(QUBIT_COUNT, 0u64);
+    let mut measurements = Vec::with_capacity(RUN_COUNT);
+    for _ in 0..RUN_COUNT {
+        simulation.init_ground_state();
+        entangle_into_bell_state(&mut simulation, 0, 1);
+        let measured_states = simulation.measure_all();
+        measurements.push(measured_states);
+    }
+    evaluate(measurements);
     println!();
 }
