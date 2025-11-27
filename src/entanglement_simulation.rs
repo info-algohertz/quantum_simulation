8a97837c78aed03d2c58acef3cf250ae097259c2
David Natingga <info@algohertz.com>
2025-11-27 11:11:21 +0100

    Add TODO to entanglement simulation, make compilable.

--- src/entanglement_simulation.rs
+++ src/entanglement_simulation.rs
@@ -1,8 +1,10 @@
 /*
+TODO: complete the implementation.
+
 Quantum simulation based on simulating an entanglement with a virtual qubit.
 
 An advantage of this simulation is that it potentially occupies less memory,
-proportional only to the number of the qubits plus the number of the entanglements betweent hem.
+proportional only to the number of the qubits plus the number of the entanglements betweent them.
 
 Copyright © 2024 AlgoHertz. All rights reserved.
 */
@@ -75,6 +77,42 @@ impl QuantumSimulation {
 
     pub fn pauli_x(&mut self, qubit_number: usize) {
         self.apply_one_qubit_gate(gate::pauli_x, qubit_number);
+    }
+
+    fn apply_two_qubit_gate<F>(
+        &mut self,
+        two_qubit_gate: F,
+        qubit_number0: usize,
+        qubit_number1: usize,
+    ) where
+        F: Fn(
+            Complex<f64>,
+            Complex<f64>,
+            Complex<f64>,
+            Complex<f64>,
+        ) -> (Complex<f64>, Complex<f64>, Complex<f64>, Complex<f64>),
+    {
+        panic!("Not implemented!");
+        assert!(
+            (qubit_number0 < self.qubit_count) && (qubit_number1 < self.qubit_count),
+            "The qubit number has to be less than the number of qubits {}.",
+            self.qubit_count
+        );
+    
+        /*
+        let new_state0: Vec<(f64, usize)> = vec![];
+        let new_state1: Vec<(f64, usize)> = vec![];
+        for j0 in 0..self.qubit_states[qubit_number0].len() {
+            for j1 in 0..self.qubit_states[qubit_number1].len() {
+                let prob0, vq_ref0 = self.qubit_states[qubit_number0][j0];
+                let prob1, vq_ref1 = self.qubit_states[qubit_number1][j1];
+                let prob = prob0*prob1;
+                let (a0, b0) = self.virt_qubits[vq_ref0];
+                let (a1, b1) = self.virt_qubits[vq_ref1];
+                two_qubit_gate
+            }
+        }
+        */
     }
 
 }
