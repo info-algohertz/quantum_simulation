edeb31782999f66cf387359101f0229c970f6e38
David Natingga <info@algohertz.com>
2025-12-02 06:15:02 +0100

    Add more examples for Deutsch-Jozsa Algorithm

--- src/bin/deutsch_jozsa_balanced.rs
+++ src/bin/deutsch_jozsa_balanced.rs
@@ -49,6 +49,15 @@ where
     simulation.measure(Vec::from(input_qubits))
 }
 
+fn is_constant(measurements: &Vec<bool>) -> bool {
+    for i in 0..measurements.len() {
+        if measurements[i] {
+            return false;
+        }
+    }
+    true
+}
+
 fn run_deutsch_jozsa_algo<const N: usize, F>(f: F, run_count: usize)
 where
     F: Fn([bool; N]) -> bool,
@@ -56,20 +65,65 @@ where
     let mut simulation = QuantumSimulation::new(N + 1, 0u64);
     let mut measurements = Vec::with_capacity(RUN_COUNT);
     let input_qubits: [usize; N] = std::array::from_fn(|i| i);
+    let mut constant_count = 0usize;
     for _ in 0..run_count {
         simulation.reset();
         let measured_states = apply_deutsch_jozsa_algo(&mut simulation, input_qubits, N, &f);
+        if is_constant(&measured_states) {
+            constant_count += 1;
+        }
         measurements.push(measured_states);
+    }
+
+    if constant_count == run_count {
+        println!("The function is constant.");
+    } else if constant_count == 0 {
+        println!("The function is balanced.");
+    } else {
+        // Such function can still end up with some small probability in the constant or balanced category.
+        println!("The function is neither constant nor balanced.");
     }
     evaluate(measurements);
 }
 
-fn true_function(_x: [bool; 4]) -> bool {
+fn false_function(_x: [bool; 4]) -> bool {
+    false
+}
+
+fn true_function(_x: [bool; 8]) -> bool {
     true
 }
 
+fn projection_function(x: [bool; 6]) -> bool {
+    x[0]
+}
+
+fn xor_function(x: [bool; 4]) -> bool {
+    x[0] ^ x[1] ^ x[2] ^ x[3]
+}
+
+fn and_function(x: [bool; 4]) -> bool {
+    x[0] & x[1] & x[2] & x[3]
+}
+
 fn main() {
-    println!("Deutsch-Jozsa algorithm on constant 1 function");
+    println!("Deutsch-Jozsa algorithm on Constant 0 function");
+    run_deutsch_jozsa_algo(false_function, RUN_COUNT);
+    println!();
+
+    println!("Deutsch-Jozsa algorithm on Constant 1 function");
     run_deutsch_jozsa_algo(true_function, RUN_COUNT);
+    println!();
+
+    println!("Deutsch-Jozsa algorithm on Projection function");
+    run_deutsch_jozsa_algo(projection_function, RUN_COUNT);
+    println!();
+
+    println!("Deutsch-Jozsa algorithm on XOR function");
+    run_deutsch_jozsa_algo(xor_function, RUN_COUNT);
+    println!();
+
+    println!("Deutsch-Jozsa algorithm on AND function");
+    run_deutsch_jozsa_algo(and_function, RUN_COUNT);
     println!();
 }
